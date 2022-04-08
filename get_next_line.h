983f6a9d04b19e3f87d2e392a28e01aa6c1cc747
eisikogl <98874999+eisikogl@users.noreply.github.com>
2022-04-08 22:27:59 +0300

    finished project

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE 
+#  define BUFFER_SIZE //1000
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char			*get_next_line(int fd);
+char			*ft_get_line(char *save);
+char			*ft_save(char *save);
+char			*ft_read_and_save(int fd, char *save);
+
+
+size_t			ft_strlen(char *str);
+char			*ft_strchr(char *s, int c);
+char			*ft_strjoin(char *s1, char *s2);
+
+#endif
